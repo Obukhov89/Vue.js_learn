cbbc76338631990e052073f1b6a88a9fe1371660
Obukhov89 <75534740+Obukhov89@users.noreply.github.com>
2021-05-16 15:35:19 +0500

    Add files via upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,30 @@
 <template>
   <div class="hello">
-    <input v-model.number="operand1" />
+    <input v-model.number="operand1"  />
     <input v-model.number="operand2" />
     ={{ result }}
-    <div>
+    <div class="operator">
       <button @click="operations('+')">+</button>
       <button @click="operations('-')">-</button>
       <button @click="operations('*')">*</button>
-      <button @click="operations('/')">/</button> 
+      <button @click="operations('/')">/</button>
       <button @click="operations('%')">%</button>
     </div>
-    <div>
-      <input v-model.number="operand3"/>
-      <input v-model.number="pow"/>
-     = {{resPow}}
+    <div class="operand">
+      <input v-model.number="operand3" />
+      <input v-model.number="pow" />
+      = {{ resPow }}
     </div>
     <div>
-      <button @click="operations('**')">**</button> 
-      
+      <button @click="operations('**')">**</button>
+    </div>
+    <div class="checkPanel">
+      <input type="checkbox" @click="visible=!visible" id="check"/>
+      <label class="laabelForPanel" for="check">Отобразить экранную клавиатуру.</label>
+      <div v-show="visible" class="groupNumbers">
+        <button  v-for="item in buttons" v-bind:key="item.id" >{{item.title}}</button>
+         <button>&#8592;</button>
+      </div>
     </div>
   </div>
 </template>
@@ -27,29 +34,30 @@ export default {
   data: () => ({
     operand1: null,
     operand2: null,
-    operand3 : null,
+    operand3: null,
     pow: null,
     result: null,
     resPow: null,
-   
+    buttons: [
+      {id:1,title:'0'},
+      {id:2,title:'1'},
+      {id:3,title:'2'},
+      {id:4,title:'3'},
+      {id:5,title:'4'},
+      {id:6,title:'5'},
+      {id:7,title:'6'},
+      {id:8,title:'7'},
+      {id:9,title:'8'},
+      {id:10,title:'9'}
+    ],
+    visible : false,
   }),
 
   props: {},
   methods: {
-    // sum(){
-    //   this.result = this.operand1 + this.operand2
-    // },
-    // sub(){this.result = this.operand1 - this.operand2
-    // },
-    // mul(){
-    //   this.result = this.operand1 * this.operand2
-    // },
-    // div(){
-    //   this.result = this.operand1 / this.operand2
-    // }
- operations: function (operator) {
+    operations: function(operator) {
       switch (operator) {
-        case '+':
+        case "+":
           this.result = +this.operand1 + +this.operand2;
           break;
         case "-":
@@ -61,17 +69,19 @@ export default {
         case "*":
           this.result = +this.operand1 * +this.operand2;
           break;
-        case"**":
-        this.resPow= this.operand3 ** +this.pow;
-        break;
+        case "**":
+          this.resPow = this.operand3 ** +this.pow;
+          break;
         case "%":
-          this.result= +this.operand1 % +this.operand2;
+          this.result = +this.operand1 % +this.operand2;
         default:
           this.result = "invalid operator";
-
       }
-    }
-  },
+    },
+   },
+   clog(element){
+     console.log(element.target.value)
+   }
 };
 </script>
 
@@ -90,5 +100,24 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button {
+  margin: 10px;
+  width: 50px;
+  height: 50px;
+}
+.operator {
+  margin: 10px;
+}
+
+.groupNumbers {
+  /* display: none; */
+  
+  margin-left: 10px;
+}
+
+.operand {
+  margin: 10px;
 }
 </style>
